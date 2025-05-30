632741d74d102ff279ba54bb1ce1b81436b66dd1
Mauro Daisuke <mauro.daisuke@hotmail.com>
2025-05-29 21:22:05 -0400

    add and fix: changed prismaclient path and add route in app

--- src/index.js
+++ src/index.js
@@ -1,19 +1,28 @@
 import dotenv from "dotenv";
 import express from "express";
 import cors from "cors";
+import { userRouter } from "./routes/userRoutes.js";
+import { authRouter } from "./routes/authRoutes.js";
 
-// import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "./generated/prisma/index.js";
 
 dotenv.config();
 
 const app = express();
 
-// const prisma = new PrismaClient();
+const prisma = new PrismaClient();
 
 app.use(cors());
 app.use(express.json());
 app.use("uploads", express.static("src/uploads"));
 
-app.get("/ping", (req, res) => res.send("🛠️ API está online!"));
+app.use("/api/auth", authRouter);
+app.use("/api/user", userRouter);
+app.get("/", (req, res) => res.json("API rodando"));
+// app.use("/api/order", orderRoutes);
 
 const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Conectado na porta ${PORT}`);
+});
