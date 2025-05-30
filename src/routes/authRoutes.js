e0c2e168c64a39e4b8f2f14e9f36e7c195dde8c4
Mauro Daisuke <mauro.daisuke@hotmail.com>
2025-05-29 21:46:32 -0400

    fix: renamed authController to loginController

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { login } from "../controllers/authController.js";
+import { login } from "../controllers/loginController.js";
 
 export const authRouter = express.Router();
 
