43d57e780f07afc45ad94cf1d21ff742e6dc6b9c
Mauro Daisuke <mauro.daisuke@hotmail.com>
2025-06-01 00:05:40 -0400

    fix: fixed token bug for validation

--- src/middlewares/authMiddleware.js
+++ src/middlewares/authMiddleware.js
@@ -4,19 +4,15 @@ export const verifyToken = (req, res, next) => {
   const token = req.headers["authorization"];
 
   if (!token)
-    return res.status(403).json({ status: false, message: "Token inválido" });
+    return res
+      .status(403)
+      .json({ status: false, message: "Token inválido ou não fornecido." });
 
-  jwt.verify(
-    token.replace("Bearer", ""),
-    process.env.JWT_SECRET,
-    (err, decoded) => {
-      if (err)
-        return res
-          .status(401)
-          .json({ status: false, message: "Token inválido" });
+  jwt.verify(token.split(" ")[1], process.env.JWT_SECRET, (err, decoded) => {
+    if (err)
+      return res.status(401).json({ status: false, message: "Token inválido" });
 
-      req.employee = decoded;
-      next();
-    }
-  );
+    req.employee = decoded;
+    next();
+  });
 };
