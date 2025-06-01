46aa60d43b5a9d14082dd1771c65a44ee7f68fe6
Mauro Daisuke <mauro.daisuke@hotmail.com>
2025-06-01 01:24:43 -0400

    feature: implemented zod on login

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -2,15 +2,22 @@ import dotenv from "dotenv";
 import jwt from "jsonwebtoken";
 
 import { PrismaClient } from "../generated/prisma/index.js";
+import { loginSchema } from "../validations/schema.js";
 
 dotenv.config();
 
 const prisma = new PrismaClient();
 
 export const login = async (req, res) => {
-  const { username, password } = req.body;
-
   try {
+    const validateLogin = loginSchema.safeParse(req.body);
+
+    if (!validateLogin.success)
+      return res
+        .status(400)
+        .json({ status: false, message: "Erro de validação." });
+
+    const { username, password } = req.body;
     const employeeExist = await prisma.employee.findUnique({
       where: { username },
     });
