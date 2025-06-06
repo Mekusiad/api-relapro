fb25ba7204d9ae283790cf90a2e968f565c41e82
Mauro Daisuke <mauro.daisuke@hotmail.com>
2025-06-05 20:49:17 -0400

    adicionado nova ordem e refatorado alguns códigos.

--- src/controllers/ordemController.js
+++ src/controllers/ordemController.js
@@ -8,137 +8,42 @@ import {
 } from "../services/ordemServices.js";
 import { handleError } from "../utils/errorHandler.js";
 
-export const novaOrdem = async (req, res) => {
-  const {
-    cliente,
-    nomeResponsavel,
-    contato,
-    email,
-    localServico,
-    descricaoInicial,
-    previsaoInicio,
-    supervisorMatricula,
-    tecnicoMatricula,
-    nivelAcesso,
-  } = req.body;
+export const novaOrdemController = async (req, res) => {
+  const data = req.body;
 
   try {
-    const osExist = await prisma.ordem.findFirst({
-      where: {
-        cliente,
-        localServico,
-        previsaoInicio,
-        descricaoInicial,
-      },
-    });
-
-    if (osExist)
-      return res.status(409).json({
-        status: false,
-        message: `Já existe uma ordem de serviço com os mesmos dados: ${osExist.numeroOs}`,
-      });
-
-    if (nivelAcesso === "técnico")
-      return res.status(403).json({
-        status: false,
-        message: "Usuário não autorizado a criar chamado.",
-      });
-
-    const now = new Date();
-
-    const ano = now.getFullYear(); // ano vigente
-    const mes = String(now.getMonth() + 1).padStart(2, "0"); // mês vigente
-
-    const prefixo = `${ano}${mes}`; // cocatena ano+mês
-
-    // Conta quantas OS já existem para o mês atual
-    const countMes = await prisma.ordem.count({
-      where: {
-        createdAt: {
-          gte: new Date(`${ano}-${mes}-01T00:00:00.000Z`),
-          lt: new Date(
-            `${ano}-${String(Number(mes) + 1).padStart(
-              2,
-              "0"
-            )}-01T00:00:00.000Z`
-          ),
-        },
-      },
-    });
-
-    const numeroSequencial = String(countMes + 1).padStart(3, "0"); // gera o próximo n° disponível do mês
-    const numberOs = `${prefixo}${numeroSequencial}`; // cocatena com ano+mês+n°disponível do mês
-    const novaOS = await prisma.ordem.create({
-      data: {
-        numeroOs: numberOs,
-        cliente,
-        nomeResponsavel,
-        contato,
-        email,
-        localServico,
-        descricaoInicial,
-        previsaoInicio: new Date(previsaoInicio),
-        supervisor: {
-          connect: { matricula: supervisorMatricula },
-        },
-        tecnico: {
-          connect: tecnicoMatricula.map((matricula) => ({
-            matricula,
-          })),
-        },
-        status: "aberta",
-      },
-      include: {
-        supervisor: {
-          select: {
-            nome: true,
-            matricula: true,
-          },
-        },
-        tecnico: {
-          select: {
-            nome: true,
-            matricula: true,
-          },
-        },
-      },
-    });
-    res.status(201).json(novaOS);
-  } catch (err) {
-    console.log(err);
-    res.status(400).json({ error: "Erro ao criar Ordem de Serviço" });
+    await novaOrdem(data);
+  } catch (error) {
+    return handleError(res, error, "Error ao criar ordem de serviço.");
   }
 };
 
 export const atualizarOrdemController = async (req, res) => {
-  // Campo para realizar  a parte de validação schema
   const { numeroOs } = req.params;
   const { type, data } = req.body;
 
   try {
     switch (type) {
       case "add-tecnico":
-        const added = await adicionarTecnico(numeroOs, data.tecnicoMatricula);
-        return res.status(200).json({
-          status: true,
-          message: "Técnico adicionado com sucesso.",
-          data: added.tecnico,
-        });
+        try {
+          await adicionarTecnico(numeroOs, data.tecnicoMatricula, res);
+        } catch (error) {
+          return handleError(res, error, "Erro ao adicionar técnico na OS.");
+        }
 
       case "remove-tecnico":
-        const removed = await removerTecnico(numeroOs, data.tecnicoMatricula);
-        return res.status(200).json({
-          status: true,
-          message: "Técnico removido com sucesso.",
-          data: removed.tecnico,
-        });
+        try {
+          await removerTecnico(numeroOs, data.tecnicoMatricula, res);
+        } catch (error) {
+          return handleError(res, error, "Erro ao remover técnico da OS.");
+        }
 
       case "trocar-supervisor":
-        await trocarSupervisor(numeroOs, data.supervisorMatricula);
-        return res.status(200).json({
-          status: true,
-          message: "Supervisor trocado com sucesso.",
-        });
+        try {
+          await trocarSupervisor(numeroOs, data.supervisorMatricula, res);
+        } catch (error) {
+          return handleError(res, error, "Erro ao trocar supervisor da OS.");
+        }
 
       default:
         return res.status(400).json({
@@ -147,53 +52,35 @@ export const atualizarOrdemController = async (req, res) => {
         });
     }
   } catch (error) {
-    return handleError(res, error, error.message);
+    return handleError(res, error, "Erro interno no servidor.");
   }
 };
 
 export const adicionarComponenteController = async (req, res) => {
-  const { numeroOs } = req.params;
-
   try {
     const { data } = req.body;
-    const novoComponente = await adicionarComponente(numeroOs, data);
-    return res
-      .status(201)
-      .json(
-        { status: true, message: "Componente adicionado com sucesso." },
-        novoComponente
-      );
+    const { numeroOs } = req.params;
+    await adicionarComponente(numeroOs, data);
   } catch (error) {
-    return handleError(res, error, error.message);
+    return handleError(res, error, "Erro ao adicionar componente");
   }
 };
 
 export const atualizarComponenteController = async (req, res) => {
-  const { numeroOs, numeroSerie } = req.params;
-
   try {
-    const comoponenteAtualizado = await atualizarComponente(numeroOs, data);
-    return res
-      .status(200)
-      .json(
-        { status: true, message: "Componente atualizado com sucesso." },
-        comoponenteAtualizado
-      );
+    const { numeroOs, numeroSerie } = req.params;
+    const { data } = req.body;
+    await atualizarComponente(numeroOs, numeroSerie, data);
   } catch (error) {
     return handleError(res, error, error.message);
   }
 };
 
 export const listarOrdemController = async (req, res) => {
-  const { numeroOs } = req.params;
-  console.log(numeroOs);
+  const { status } = req.query;
 
   try {
-    const osExist = await listarServicos(numeroOs);
-
-    return res
-      .status(200)
-      .json({ status: true, message: "Ordem localizada.", osExist });
+    await listarServicos(status);
   } catch (error) {
     return handleError(res, error, error.message);
   }
--- src/services/ordemServices.js
+++ src/services/ordemServices.js
@@ -2,7 +2,7 @@ import { PrismaClient } from "../generated/prisma/index.js";
 
 const prisma = new PrismaClient();
 
-export const adicionarTecnico = async (numeroOs, tecnicoMatricula) => {
+export const adicionarTecnico = async (numeroOs, tecnicoMatricula, res) => {
   const ordem = await prisma.ordem.findUnique({
     where: { numeroOs },
     include: { tecnico: true },
@@ -11,9 +11,12 @@ export const adicionarTecnico = async (numeroOs, tecnicoMatricula) => {
   const alreadyExists = ordem.tecnico.some(
     (t) => t.matricula === tecnicoMatricula
   );
-  if (alreadyExists) throw new Error("Técnico já está vinculado à OS.");
+  if (alreadyExists)
+    return res
+      .status(400)
+      .json({ status: false, message: "Técnico já está vinculado à OS." });
 
-  return await prisma.ordem.update({
+  const adicionado = await prisma.ordem.update({
     where: { numeroOs },
     data: {
       tecnico: {
@@ -22,9 +25,15 @@ export const adicionarTecnico = async (numeroOs, tecnicoMatricula) => {
     },
     include: { tecnico: { select: { nome: true, matricula: true } } },
   });
+
+  return res.status(200).json({
+    status: true,
+    message: "Técnico adicionado com sucesso.",
+    data: adicionado.tecnico,
+  });
 };
 
-export const removerTecnico = async (numeroOs, tecnicoMatricula) => {
+export const removerTecnico = async (numeroOs, tecnicoMatricula, res) => {
   const ordem = await prisma.ordem.findUnique({
     where: { numeroOs },
     include: { tecnico: true },
@@ -33,7 +42,7 @@ export const removerTecnico = async (numeroOs, tecnicoMatricula) => {
   const exists = ordem.tecnico.some((t) => t.matricula === tecnicoMatricula);
   if (!exists) throw new Error("Técnico não está vinculado à OS.");
 
-  return await prisma.ordem.update({
+  const removido = await prisma.ordem.update({
     where: { numeroOs },
     data: {
       tecnico: {
@@ -42,16 +51,24 @@ export const removerTecnico = async (numeroOs, tecnicoMatricula) => {
     },
     include: { tecnico: { select: { nome: true, matricula: true } } },
   });
+
+  return res.status(200).json({
+    status: true,
+    message: "Técnico removido com sucesso.",
+    data: removido.tecnico,
+  });
 };
 
-export const trocarSupervisor = async (numeroOs, supervisorMatricula) => {
+export const trocarSupervisor = async (numeroOs, supervisorMatricula, res) => {
   const supervisor = await prisma.funcionario.findUnique({
     where: { matricula: supervisorMatricula },
   });
 
-  if (!supervisor || supervisor.nivelAcesso !== "supervisor") {
-    throw new Error("Matrícula informada não pertence a um supervisor.");
-  }
+  if (!supervisor || supervisor.nivelAcesso !== "supervisor")
+    return res.status(401).json({
+      status: false,
+      message: "Matrícula informada não pertence a um supervisor.",
+    });
 
   const ordem = await prisma.ordem.findUnique({
     where: { numeroOs },
@@ -60,9 +77,11 @@ export const trocarSupervisor = async (numeroOs, supervisorMatricula) => {
 
   const atualMatricula = ordem.supervisor?.matricula;
 
-  if (atualMatricula === supervisorMatricula) {
-    throw new Error("Supervisor informado já está vinculado.");
-  }
+  if (atualMatricula === supervisorMatricula)
+    return res.status(401).json({
+      status: false,
+      message: "Supervisor informado já está vinculado.",
+    });
 
   return await prisma.ordem.update({
     where: { numeroOs },
@@ -77,42 +96,160 @@ export const trocarSupervisor = async (numeroOs, supervisorMatricula) => {
 
 export const adicionarComponente = async (numeroOs, data) => {
   const { numeroSerie } = data;
-  const existing = await prisma.componente.findFirst({
+  const componenteExiste = await prisma.componente.findFirst({
     where: { numeroSerie, ordemOs: numeroOs },
   });
-  if (existing)
-    throw new Error(
-      "Já existe um componente com esse número de série nesta OS."
-    );
+  if (componenteExiste)
+    return res.status(400).json({
+      status: false,
+      message: "Já existe um componente com esse número de série nesta OS.",
+    });
 
-  return await prisma.componente.create({
+  const componenteCriado = await prisma.componente.create({
     data: {
       ...data,
       ordem: { connect: { numeroOs } },
     },
   });
+  return res.status(201).json({
+    status: false,
+    message: "Componente cadastrado com sucesso.",
+    data: componenteCriado,
+  });
 };
 
-export const atualizarComponente = async (numeroOs, data) => {
-  const { numeroSerie } = data;
+export const novaOrdem = async (data) => {
+  const {
+    cliente,
+    nomeResponsavel,
+    contato,
+    email,
+    localServico,
+    descricaoInicial,
+    previsaoInicio,
+    supervisorMatricula,
+    tecnicoMatricula,
+    nivelAcesso,
+  } = data;
+
+  const ordemExist = await prisma.ordem.findFirst({
+    where: {
+      cliente,
+      localServico,
+      previsaoInicio,
+      descricaoInicial,
+    },
+  });
+
+  if (ordemExist)
+    return res.status(409).json({
+      status: false,
+      message: `Já existe uma ordem de serviço com os mesmos dados: ${ordemExist.numeroOs}`,
+    });
+
+  if (nivelAcesso === "TECNICO")
+    return res.status(403).json({
+      status: false,
+      message: "Usuário não autorizado a criar chamado.",
+    });
+
+  const now = new Date();
+
+  const ano = now.getFullYear(); // ano vigente
+  const mes = String(now.getMonth() + 1).padStart(2, "0"); // mês vigente
+
+  const prefixo = `${ano}${mes}`; // cocatena ano+mês
+
+  // Conta quantas OS já existem para o mês atual
+  const countMes = await prisma.ordem.count({
+    where: {
+      createdAt: {
+        gte: new Date(`${ano}-${mes}-01T00:00:00.000Z`),
+        lt: new Date(
+          `${ano}-${String(Number(mes) + 1).padStart(2, "0")}-01T00:00:00.000Z`
+        ),
+      },
+    },
+  });
+
+  const numeroSequencial = String(countMes + 1).padStart(3, "0"); // gera o próximo n° disponível do mês
+  const numberOs = `${prefixo}${numeroSequencial}`; // cocatena com ano+mês+n°disponível do mês
+  const novaOS = await prisma.ordem.create({
+    data: {
+      numeroOs: numberOs,
+      cliente,
+      nomeResponsavel,
+      contato,
+      email,
+      localServico,
+      descricaoInicial,
+      previsaoInicio: new Date(previsaoInicio),
+      supervisor: {
+        connect: { matricula: supervisorMatricula },
+      },
+      tecnico: {
+        connect: tecnicoMatricula.map((matricula) => ({
+          matricula,
+        })),
+      },
+      status: "ABERTA",
+    },
+    include: {
+      supervisor: {
+        select: {
+          nome: true,
+          matricula: true,
+        },
+      },
+      tecnico: {
+        select: {
+          nome: true,
+          matricula: true,
+        },
+      },
+    },
+  });
+
+  return res.status(201).json({
+    status: true,
+    message: "Ordem de serviço criada com sucesso",
+    data: novaOS,
+  });
+};
+
+export const atualizarComponente = async (numeroOs, numeroSerie, data) => {
   const existing = await prisma.componente.findFirst({
     where: { numeroSerie, ordemOs: numeroOs },
   });
-  if (!existing) throw new Error("Componente não encontrado nesta OS.");
+  if (!existing)
+    res
+      .status(400)
+      .json({ status: false, message: "Componente não encontrado nesta OS." });
 
-  return await prisma.trafo.update({
+  const componenteAtualizado = await prisma.trafo.update({
     where: { id: existing.id },
     data,
   });
+
+  return res.status(200).json({
+    status: true,
+    message: "Componente atualizado com sucesso.",
+    daa: componenteAtualizado,
+  });
 };
 
-export const listarServicos = async (numeroOs) => {
-  const existing = await prisma.ordem.findMany();
+export const listarServicos = async (status) => {
+  const existing = await prisma.ordem.findMany({
+    where: status ? { status } : {},
+    orderBy: { createdAt: "desc" },
+  });
 
   if (!existing)
     return res
       .status(400)
       .json({ status: false, message: "OS não encontrada e/ou não existe." });
 
-  return existing;
+  return res
+    .status(200)
+    .json({ status: true, message: "Ordem localizada.", data: existing });
 };
