1f514aec53f7b36ef78fc9681c3bcdb20d0bac51
Mauro Daisuke <mauro.daisuke@hotmail.com>
2025-08-04 19:58:22 -0400

    Ajustado upload de fotos.

--- src/middlewares/uploadMiddleware.js
+++ src/middlewares/uploadMiddleware.js
@@ -5,20 +5,27 @@ import cloudinary from "../utils/cloudinary.js";
 
 const storage = new CloudinaryStorage({
   cloudinary,
-  params: {
-    folder: "os-fotos",
-    allowed_formats: ["jpg", "jpeg", "png", "webp"],
-    transformation: [{ width: 1600, height: 1600, crop: "limit" }],
-  },
+  params: async (req, file) => {
+    const [campo, numeroOs] = file.fieldname.split("__");
+
+    return {
+      folder: `os-fotos/${numeroOs}`,
+      allowed_formats: ["jpg", "jpeg", "png", "webp"],
+      transformation: [{ width: 1600, height: 1600, crop: "limit" }],
+    };
+  }
 });
 
 export const upload = multer({ storage });
 
 
-export const uploadFields = upload.fields([
-  { name: "fotosIniciaisInput", maxCount: 5 },   
-  { name: "fotosAntesInput", maxCount: 5 },       
-  { name: "fotosDuranteInput", maxCount: 5 },     
-  { name: "fotosDepoisInput", maxCount: 5 },      
-  { name: "fotosMedicaoInput", maxCount: 5 },     
-]);
+export const uploadFields = upload.any();
+
+// export const uploadFields = upload.fields([
+//   { name: "fotosIniciaisInput", maxCount: 5 },   
+//   { name: "fotosAntesInput", maxCount: 5 },       
+//   { name: "fotosDuranteInput", maxCount: 5 },     
+//   { name: "fotosDepoisInput", maxCount: 5 },      
+//   { name: "fotosPontoAtencaoInput", maxCount: 5 },     
+//   { name: "fotosMedicaoInput", maxCount: 5 },     
+// ]);
